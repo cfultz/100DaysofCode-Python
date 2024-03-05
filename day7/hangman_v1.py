c04150de65e77dde48d126a6d415808966ea0cbb
Caleb Fultz <caleb@fultz.dev>
2024-03-05 13:20:09 -0500

    updated game logic

--- day7/hangman_v1.py
+++ day7/hangman_v1.py
@@ -2,17 +2,25 @@ import random
 
 word_list = ["aardvark", "baboon", "camel"]
 
-#TODO-1 - Randomly choose a word from the word_list and assign it to a variable called chosen_word.
+#TODO-1 
 
 chosen_word = random.choice(word_list)
+display = []
+word_length = len(chosen_word)
+for _ in range(len(chosen_word)):
+    display += "_"
 
-#TODO-2 - Ask the user to guess a letter and assign their answer to a variable called guess. Make guess lowercase.
+#TODO-2 
+
+print(display)
 
 guess = input("Select a letter: ").lower()
 
-#TODO-3 - Check if the letter the user guessed (guess) is one of the letters in the chosen_word.
+#TODO-3 
 
-if guess in chosen_word:
-    print("Yes!")
-else:
-    print("No!")
+for position in range(word_length):
+    letter = chosen_word[position]
+    if letter == guess:
+        display[position] = letter
+
+print(display)
